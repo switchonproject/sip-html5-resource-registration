c6980c96ad0181d7dc231986e0bc4621b503b935
Pascal Dihé <pascal@cismet.de>
2016-01-26 17:27:36 +0100

    #5 map selection

--- app/scripts/controllers/geoLocationController.js
+++ app/scripts/controllers/geoLocationController.js
@@ -4,33 +4,104 @@ angular.module(
     'de.cismet.sip-html5-resource-registration.controllers.geoLocationController',
     [
         '$scope',
+        '$timeout',
         'AppConfig',
-        'WizardHandler',
         'leafletData',
         'de.cismet.sip-html5-resource-registration.services.dataset',
-        'de.cismet.sip-html5-resource-registration.services.TagGroupService',
+        'de.cismet.sip-html5-resource-registration.services.CountriesService',
         // Controller Constructor Function
         function (
             $scope,
+    $timeout,
             AppConfig,
-            WizardHandler,
             leafletData,
             dataset,
-            tagGroupService
+            countriesService
         ) {
             'use strict';
 
-            var _this, config, fireResize, southWest, northEast, maxBounds;
+            var _this, config, fireResize, southWest, northEast, maxBounds,
+                    drawControls, layerGroup, wicket, defaultStyle, defaultDrawOptions,
+                    noDrawOptions, writeSpatialCoverage,
+                    readSpatialCoverage, drawControlsEnabled;
             _this = this;
-            
-            
+
             _this.config = AppConfig;
             
             /**
              * overall Form data
              */
-            //_this.dataset = dataset;
+            _this.dataset = dataset;
             
+            $scope.scountry = {};
+            
+            _this.contentLocation = {};
+            _this.contentLocation.name = 'Belarus';
+            _this.contentLocation.type = null;
+            _this.contentLocation.wkt = null;
+            _this.contentLocation.layer = null;
+            
+            _this.countries = [];
+            _this.countries['countries-world'] = countriesService.getCountryList('countries-world'); 
+            _this.countries['countries-europe'] = countriesService.getCountryList('countries-europe');
+            
+            //$timeout(function(){
+            //    _this.countries['countries-world'] = countriesService.getCountryList('countries-world'); 
+            //    _this.countries['countries-europe'] = countriesService.getCountryList('countries-europe'); 
+            //},3000);
+            
+           _this.onSelectedCountry = function(item) {
+                wicket.read(item.wkt);
+                var layer = wicket.toObject(defaultStyle);
+               // _this.contentLocation.layer = layer;
+               
+                layerGroup.clearLayers();
+                layerGroup.addLayer(layer);
+                
+                leafletData.getMap('mainmap').then(function (map) {
+                        map.fitBounds(layerGroup.getBounds(), {
+                            animate: true,
+                            pan: {animate: true, duration: 0.6},
+                            zoom: {animate: true}
+                        });
+                   });
+                   
+                   _this.contentLocation = Object.create(item);
+                   _this.contentLocation.layer = layer;
+           };
+            
+            wicket = new Wkt.Wkt();
+            defaultStyle = {color: '#0000FF', fillOpacity: 0.3, weight: 2, fill: true, fillColor: '#1589FF', riseOnHover: true, clickable: true};
+            
+            defaultDrawOptions = {
+                    polyline: false,
+                    polygon: {
+                        shapeOptions: defaultStyle,
+                        showArea: true,
+                        metric: true,
+                        allowIntersection: false,
+                        drawError: {
+                            color: '#e1e100', // Color the shape will turn when intersects
+                            message: '<strong>Oh snap!<strong> you can\'t draw that!</strong>' // Message that will show when intersect
+                        }       
+                    },
+                    rectangle: {
+                        shapeOptions: defaultStyle,
+                        metric: true
+                    },
+                    // no circles for starters as not compatible with WKT
+                    circle: false,
+                    marker: false
+                };
+                
+            noDrawOptions = { 
+                polyline: false,
+                polygon: false,
+                rectangle: false,
+                circle: false,
+                marker: false
+            };
+                
             
             /**
              * Map init data
@@ -42,11 +113,7 @@ angular.module(
                     //tileLayerOptions: {noWrap: true},
                     //maxZoom: 14,
                     minZoom: _this.config.minZoom,
-                    path: {
-                        weight: 10,
-                        color: '#800000',
-                        opacity: 1
-                    }
+                    path: defaultStyle
                 };
             
             
@@ -70,11 +137,30 @@ angular.module(
                          _this.mode[key] = false;
                      }
                   }, _this.mode);
+                  
+                // perform initialisation of widgets
+                if(_this.mode.drawBBox === true && !drawControlsEnabled) {
+                    drawControls.setDrawingOptions(defaultDrawOptions);
+                    leafletData.getMap('mainmap').then(function (map) {
+                        map.addControl(drawControls);
+                     });
+                     drawControlsEnabled = true;
+                } else if(drawControlsEnabled) {
+                    drawControls.setDrawingOptions(noDrawOptions);
+                    leafletData.getMap('mainmap').then(function (map) {
+                        map.removeControl(drawControls);
+                     });
+                     drawControlsEnabled = false;
+                }
+                
+                if(_this.mode.defineBBox === true && _this.contentLocation.layer !== null) {
+                    console.log(_this.contentLocation.layer.getBounds());
+                }
             };
             
+            // resize the map on enter
             $scope.wizard.enterValidators[$scope.wzTitle] = function(){
                 fireResize();
-                
                 return true;
             };
             
@@ -83,12 +169,37 @@ angular.module(
                 return true;
             };
             
+            // local methods and variables
+            
+            readSpatialCoverage = function(dataset) {
+                if(dataset.spatialcoverage && dataset.spatialcoverage.geo_field) { // jshint ignore:line
+                    var wktString = dataset.spatialcoverage.geo_field; // jshint ignore:line
+                    wicket.read(wktString.substr(wktString.indexOf(';') + 1));
+
+                    var layer = wicket.toObject(defaultStyle);
+                    layer.setStyle(defaultStyle);
+                    return layer;
+                }
+
+                return undefined;
+            };
+            
+            writeSpatialCoverage = function(dataset, wktString) {
+                if(wktString && dataset.spatialcoverage) { // jshint ignore:line
+                    var wktStringWithSRS = 'SRID=4326;'+wktString;
+                    dataset.spatialcoverage.geo_field = wktStringWithSRS; // jshint ignore:line
+                    wicket.read(wktString.substr(wktString.indexOf(';') + 1));
+                }
+            };
+
             fireResize = function () {
                 leafletData.getMap('mainmap').then(function (map) {
-                    setTimeout(function(){ map.invalidateSize();}, 5);
+                    setTimeout(function(){ map.invalidateSize();}, 50);
                 });
             };
             
+            
+            // leafltet initiaisation
             southWest = (_this.config.maxBounds && angular.isArray(_this.config.maxBounds.southWest)) ?
                             L.latLng(config.maxBounds.southWest[0], _this.config.maxBounds.southWest[1]) :
                             L.latLng(90, -180);
@@ -99,6 +210,62 @@ angular.module(
             leafletData.getMap('mainmap').then(function (map) {
                 map.setMaxBounds($scope.maxBounds);
             });
+            
+            //draw control initialisation
+            layerGroup = new L.FeatureGroup();
+            drawControls = new L.Control.Draw({
+                draw: defaultDrawOptions,
+                edit: {
+                    featureGroup: layerGroup
+                }
+            });
+            drawControlsEnabled = true;
+            
+            
+            
+            leafletData.getMap('mainmap').then(function (map) {
+                map.addLayer(layerGroup);
+                map.addControl(drawControls);
+                map.on('draw:created', function (event) {
+                    console.log(event.layerType + ' created'); 
+                    layerGroup.clearLayers();
+                    layerGroup.addLayer(event.layer);
+                    
+                    wicket.fromObject(event.layer);
+
+                    _this.contentLocation = {};
+                    _this.contentLocation.name = 'New ' + event.layerType;
+                    _this.contentLocation.type = event.layerType;
+                    _this.contentLocation.layer = event.layer;
+                    _this.contentLocation.wkt = wicket.write();
+                    
+                });
+                
+                map.on('draw:edited', function (event) {
+                    console.log(event.layers.getLayers().length + ' edited'); 
+                    //layerGroup.addLayer(event.layers.getLayers()[0]);
+                    
+                    wicket.fromObject(event.layers.getLayers()[0]);
+                    _this.contentLocation = Object.create(_this.contentLocation);
+                    if(_this.contentLocation.name.indexOf('(edited)') === -1) {
+                        _this.contentLocation.name += ' (edited)';
+                    }
+                    _this.contentLocation.layer = event.layers.getLayers()[0];
+                    _this.contentLocation.wkt = wicket.write();
+                });
+
+                map.on('draw:deleted', function (event) {
+                    console.log(event.layers.getLayers().length + ' deleted'); 
+                    layerGroup.clearLayers();
+                    _this.contentLocation = null;
+                });
+            });
+            
+            // local functions 
+            
+            
+            
+           
 
            
             

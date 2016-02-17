f5020782ce674b6d431f3b791790c5e84550db15
Pascal Dihé <pascal@cismet.de>
2016-02-17 14:26:53 +0100

    #10 conten type sort behaviour fixed

--- app/scripts/filters/contenttypeFilter.js
+++ app/scripts/filters/contenttypeFilter.js
@@ -5,27 +5,23 @@ angular.module(
         function () {
             'use strict';
 
-            function customOrder(description) {
-                if(description.indexOf('unknown') !== -1) {
-                    return 0;
-                }
-                
-                return 1;
-            }
 
             return function (items) {
                 var filtered = [];
                 angular.forEach(items, function (item) {
-//                    var name = item.description.split(' (', 1);
-//                    if(name.length === 1) {
-//                        item.description = name[0];
-//                    } 
-                    
                     filtered.push(item);
                 });
-                
+
                 filtered.sort(function (a, b) {
-                    return (customOrder(a.description) > customOrder(b.description) ? 1 : -1);
+
+                    if (a.description.indexOf('unknown') !== -1) {
+
+                        return -1;
+                    } else if (b.description.indexOf('unknown') !== -1) {
+                        return 1;
+                    } else {
+                        return a.description.localeCompare(b.description);
+                    }
                 });
                 return filtered;
             };

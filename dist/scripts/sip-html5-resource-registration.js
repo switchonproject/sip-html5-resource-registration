1de7e787682f2964ca2c8fc39226351be79b663c
Pascal Dihé <pascal@cismet.de>
2016-03-09 09:31:12 +0100

    #11 byod url changed

--- dist/scripts/sip-html5-resource-registration.js
+++ dist/scripts/sip-html5-resource-registration.js
@@ -1281,7 +1281,8 @@ angular.module(
         appConfig.objectInfo.resourceXmlUrl = 'http://tl-243.xtr.deltares.nl/csw?request=GetRecordById&service=CSW&version=2.0.2&namespace=xmlns%28csw=http://www.opengis.net/cat/csw/2.0.2%29&resultType=results&outputSchema=http://www.isotc211.org/2005/gmd&outputFormat=application/xml&ElementSetName=full&id=';
 
         appConfig.byod = {};
-        appConfig.byod.baseUrl = 'http://tl-243.xtr.deltares.nl/byod';
+        appConfig.byod.baseUrl = 'http://www.water-switch-on.eu/sip-webclient/byod/';
+        //appConfig.byod.baseUrl = 'http://tl-243.xtr.deltares.nl/byod';
         //appConfig.byod.baseUrl = 'http://switchon.cismet.de/sip-snapshot';
         
         appConfig.uploadtool = {};

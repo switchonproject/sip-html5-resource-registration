fd584f3f2d9c8073219dbaea6f7d685939860e03
Pascal Dihé <pascal@cismet.de>
2016-03-18 13:15:47 +0100

    #12 self updating representation factory

--- app/scripts/services/representationFactory.js
+++ app/scripts/services/representationFactory.js
@@ -10,35 +10,79 @@
 
 angular.module('de.cismet.sip-html5-resource-registration.services')
         .factory('de.cismet.sip-html5-resource-registration.services.RepresentationFactory',
-                ['$resource',
-                    function ($resource) {
+                ['de.cismet.sip-html5-resource-registration.services.TagGroupService',
+                    function (tagGroupService) {
                         'use strict';
 
-                        function Representation() {
-                            var _this, resourceTemplate;
+                        function Representation(representation) {
+                            var _this; //, resourceTemplate;
                             _this = this;
 
-                            resourceTemplate = $resource('data/representationTemplate.json', {}, {
-                                query: {
-                                    method: 'GET',
-                                    params: {
-                                    },
-                                    isArray: false
-                                }
-                            }).query();
+                            _this.$self = '/SWITCHON.REPRESENTATION/-1';
+                            _this.id = -1;
 
-                            resourceTemplate.$promise.then(function (json) {
-                                for (var key in json) {
-                                    if (json.hasOwnProperty(key) && !_this.hasOwnProperty(key) &&
+                            if (representation) {
+                                for (var key in representation) {
+                                    if (representation.hasOwnProperty(key) && !_this.hasOwnProperty(key) &&
                                             key !== '$resolved' && key !== '$promise') {
-                                        _this[key] = json[key];
+                                        _this[key] = representation[key];
                                     }
                                 }
-                            });
+                            } else {
+                                _this.contenttype = 'application/octet-stream';
+                                _this.function = 'download';
+                            } 
                         }
+                        
+                        Representation.prototype.updateTags = function () {
+                                var _this = this;
+
+                                // resource type
+                                if (!_this.type) {
+                                    _this.type = 'original data';
+                                }
+                                if (typeof _this.type !== 'object') {
+                                    tagGroupService.getTagList('representation type').$promise.then(
+                                            function (tags) {
+                                                _this.type = tags.getTagByName(_this.type);
+                                            });
+                                }
+
+                                // resource protocol
+                                if (!_this.protocol) {
+                                    _this.protocol = 'WWW:LINK-1.0-http--link';
+                                }
+                                if (typeof _this.type !== 'object') {
+                                    tagGroupService.getTagList('protocol', 'WWW:LINK-1.0-http--link,OGC:WMS-1.1.1-http-get-capabilities,WWW:TILESERVER,OPeNDAP:OPeNDAP').$promise.then(
+                                            function (tags) {
+                                                _this.protocol = tags.getTagByName(_this.protocol);
+                                            });
+                                }
+
+                                // resource function
+                                if (!_this.function) {
+                                    _this.function = 'download';
+                                }
+                                if (typeof _this.type !== 'object') {
+                                    tagGroupService.getTagList('function').$promise.then(
+                                            function (tags) {
+                                                _this.function = tags.getTagByName(_this.function);
+                                            });
+                                }
+
+                                // resource content type
+                                if (!_this.contenttype) {
+                                    _this.contenttype = 'application/octet-stream';
+                                }
+                                if (typeof _this.contenttype !== 'object') {
+                                    tagGroupService.getTagList('content type').$promise.then(
+                                            function (tags) {
+                                                _this.contenttype = tags.getTagByName(_this.contenttype);
+                                            });
+                                }
+                            };
 
                         Representation.prototype.constructor = Representation;
                         return Representation;
+
                     }]);
-
-
